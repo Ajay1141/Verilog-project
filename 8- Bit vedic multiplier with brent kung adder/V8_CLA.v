df9aafdd0d0f468d8bae6c39b6924fb1c253d08a
Ajay1141 <42061248+Ajay1141@users.noreply.github.com>
2024-03-30 13:46:23 +0530

    Add files via upload

---
+++ 8- Bit vedic multiplier with brent kung adder/V8_CLA.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04.12.2023 15:36:16
+// Design Name: 
+// Module Name: V8_CLA
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module V8_CLA(
+
+    );
+endmodule
